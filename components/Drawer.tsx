d60c7c6882cea4542ea6b8fc2fd409ecc1c80827
dd <1748420480@qq.com>
2022-03-30 17:12:20 +0800

    feat: setup env

---
+++ components/Drawer.tsx
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+const StyledBox = styled(Box)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
+  }));
+  
+  const Puller = styled(Box)(({ theme }) => ({
+    width: 30,
+    height: 6,
+    backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
+    borderRadius: 3,
+    position: 'absolute',
+    top: 8,
+    left: 'calc(50% - 15px)',
+  }));
