64184b8d692e95e2c99f1f16aa8d8c2c3b9ae0ae
dd <1748420480@qq.com>
2022-03-30 17:13:47 +0800

    wip

--- components/Drawer.tsx
+++ components/Drawer.tsx
@@ -1,16 +1,19 @@
 import styled from "@emotion/styled";
 import { Box } from "@mui/material";
+import { grey } from "@mui/material/colors";
 
 const StyledBox = styled(Box)(({ theme }) => ({
-    backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
-  }));
-  
-  const Puller = styled(Box)(({ theme }) => ({
+    backgroundColor: grey[800],
+    // backgroundColor: theme.palette.mode === 'light' ? '#fff' : grey[800],
+}));
+
+const Puller = styled(Box)(({ theme }) => ({
     width: 30,
     height: 6,
-    backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
+    backgroundColor: grey[300],
+    // backgroundColor: theme.palette.mode === 'light' ? grey[300] : grey[900],
     borderRadius: 3,
-    position: 'absolute',
+    position: "absolute",
     top: 8,
-    left: 'calc(50% - 15px)',
-  }));
+    left: "calc(50% - 15px)",
+}));
